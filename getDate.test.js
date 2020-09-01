bd212903bc9b43661b3d08a54566eade88c80e55
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2020-09-01 15:49:01 +0300

    second commit

--- getDate.test.js
+++ getDate.test.js
@@ -12,11 +12,11 @@ test('Converts working hours to the date when the work is ready', () => {
 
     expect(getDate(74,new Date(2020,8,1,14)).toString().substring(0,21)).toStrictEqual('Fri Sep 11 2020 16:00');
     //start date 2020.08.01 14:00
-    //durationHours = 1
+    //durationHours = 74
     //expected date 2020.08.11 16:00
 
     expect(getDate(794,new Date(2020,10,1,18,30)).toString().substring(0,21)).toStrictEqual('Thu Mar 04 2021 12:30');
     //start date 2020.08.01 14:00
-    //durationHours = 1
+    //durationHours = 794
     //expected date 2021.03.04 12:30
 });
